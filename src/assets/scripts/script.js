a27ffec00aad3497ff9e831550253f73a2840bba
Lucas Ribeiro <luc4s.rib3iro@gmail.com>
2019-11-21 22:20:08 -0300

    'ajustes'

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,76 @@
+
+function scrollUp() {
+    scroll(0,0);
+}
+
+function hover1(element) {
+    element.setAttribute('src', 'assets/images/IBITINGA_CORETO.png');
+}
+  
+function unhover1(element) {
+    element.setAttribute('src', 'assets/images/IBITINGA_CORETO-SELECAO.png');
+}
+
+function hover2(element) {
+    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO.png');
+}
+  
+function unhover2(element) {
+    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO-SELECAO.png');
+}
+
+function hover3(element) {
+    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA.png');
+}
+  
+function unhover3(element) {
+    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA-SELECAO.png');
+}
+
+function hover4(element) {
+    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS.png');
+}
+  
+function unhover4(element) {
+    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS-SELECAO.png');
+}
+
+function hover5(element) {
+    element.setAttribute('src', 'assets/images/CORETO_ITU.png');
+}
+  
+function unhover5(element) {
+    element.setAttribute('src', 'assets/images/CORETO_ITU-SELECAO.png');
+}
+
+function hover6(element) {
+    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO.png');
+}
+  
+function unhover6(element) {
+    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO-SELECAO.png');
+}
+
+function hover7(element) {
+    element.setAttribute('src', 'assets/images/CORNETAS.png');
+}
+  
+function unhover7(element) {
+    element.setAttribute('src', 'assets/images/CORNETAS-SELECAO.png');
+}
+
+function hover8(element) {
+    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS.png');
+}
+  
+function unhover8(element) {
+    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS-SELECAO.png');
+}
+
+function hover9(element) {
+    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU.png');
+}
+  
+function unhover9(element) {
+    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU-SELECAO.png');
+}
